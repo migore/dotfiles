411cf02abde8f9d000243ca16edf61739f10bca1
Ygor Bruxel <ygor.bruxel@gmail.com>
2020-08-05 07:23:15 +0200

    Add ubuntu boot

---
+++ ubuntu_boot.sh
@@ -0,0 +1,7 @@
+apt-get upgrade
+apt update
+apt install git npm curl xclip zsh
+
+npm install -g diff-so-fancy
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
