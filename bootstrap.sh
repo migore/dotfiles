7ea61486a9eff6775f96614b389d5aa30b1b0b8c
Ygor Bruxel <ygor.bruxel@gmail.com>
2022-03-06 16:30:41 +0100

    Small fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
 ln -s "$PWD/.gitignore" "$HOME/.gitignore"
 ln -s "$PWD/.vimrc" "$HOME/.vimrc"
 ln -s "$PWD/.myzshrc" "$HOME/.myzshrc"
-ln -s "$PWD/.tmux.cong" "$HOME/.tmux.cong"
+ln -s "$PWD/.tmux.conf" "$HOME/.tmux.conf"
 
 ln -s "$PWD/bin" "$HOME/bin"
